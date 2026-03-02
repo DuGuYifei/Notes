a09ec7c5a4c8771dbd004758c5ce98abb955ba87
DuGuYifei <liuyifei97@163.com>
2026-03-02 11:44:28 +0100

    chore: update catalog ignore python file

--- catalog_ignore.py
+++ catalog_ignore.py
@@ -2,7 +2,7 @@
 IGNORE_FILES = {
     "gitp.cmd", "gitp.sh", "gitpf.cmd", "_sidebar.md", "README.md",
     ".gitignore", ".nojekyll", "catalog.py", "catalog_graph.py", "catalog_ignore.py",
-    "index.html", "CNAME", "_404.md", "_navbar.md"
+    "index.html", "CNAME", "_404.md", "_navbar.md", "graph.html"
 }
 IGNORE_DIRS = {
     ".obsidian", ".git", "_attachments", "MyGameDesign",
